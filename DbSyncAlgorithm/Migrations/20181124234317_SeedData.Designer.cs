20e9d09a4c52cd91bf0bf264f1f06c19eb5b3b6c
ll9 <llwierer@web.de>
2018-11-25 00:50:51 +0100

    Seed Data

--- DbSyncAlgorithm/Migrations/20181124234317_SeedData.Designer.cs
+++ DbSyncAlgorithm/Migrations/20181124234317_SeedData.Designer.cs
@@ -4,14 +4,16 @@ using DbSyncAlgorithm.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DbSyncAlgorithm.Migrations
 {
     [DbContext(typeof(DbSyncAlgorithmContext))]
-    partial class DbSyncAlgorithmContextModelSnapshot : ModelSnapshot
+    [Migration("20181124234317_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DbSyncAlgorithm/Migrations/20181124234317_SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbSyncAlgorithm.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Person Values(NEWID(), 'Hans', 33, GETDATE ())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete FROM Person WHERE Name = 'Hans' AND Age = 33)");
+        }
+    }
+}

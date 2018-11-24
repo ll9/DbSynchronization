6ecbb54bfc3ac190423c30e0e6bbe795b082f756
ll9 <llwierer@web.de>
2018-11-24 23:37:13 +0100

    Server Model

---
+++ DbSyncAlgorithm/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbSyncAlgorithm.Models
+{
+    public class Person
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private DateTime? dateCreated = null;
+        public DateTime DateCreated
+        {
+            get
+            {
+                return dateCreated.HasValue
+                   ? dateCreated.Value
+                   : DateTime.Now;
+            }
+
+            set { dateCreated = value; }
+        }
+
+    }
+}

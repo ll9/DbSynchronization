2d97993fd97ff08650db7e1f1b3451553e6d3904
ll9 <llwierer@web.de>
2018-11-25 00:42:30 +0100

    basic rest

---
+++ Client/Utils/Rest.cs
@@ -0,0 +1,22 @@
+﻿using Client.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Utils
+{
+    class Rest
+    {
+        public static IList<Person> GetPeople()
+        {
+            var client = new RestClient("http://example.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("resource/{id}", Method.GET);
+            var response = client.Execute<List<Person>>(request);
+
+            return response.Data;
+        }
+    }
+}
